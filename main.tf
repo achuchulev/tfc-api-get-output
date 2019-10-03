bdc4600a1cff7192e54b8d29c418d2185d7e12c4
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-10-03 10:40:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "random_pet" "random_name" {
   length    = var.words_number
   separator = "-"
 }
+
+resource "null_resource" "echo" {
+  count = var.words_number
+
+  provisioner "local-exec" {
+    command = "echo hello ${count.index + 1}!"
+  }
+}
