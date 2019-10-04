4cb72f9b5d0ee0c36295201d102157c2dd71afca
Atanas Chuchulev <42144722+achuchulev@users.noreply.github.com>
2019-10-04 13:34:15 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "words_number" {
   description = "The number of words to contains"
   default = 4
 }
+
+variable "org" {
+  description = "The name of the TFC Organization"
+}
+
+variable "ws" {
+  description = "The name of the TFC workspace"
+}
